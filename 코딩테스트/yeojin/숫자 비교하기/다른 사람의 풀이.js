fac198b1d1b8bdd65ec7369636538ad6c4cf24e6
xy-jxn <s24016@gsm.hs.kr>
2024-08-15 21:23:30 +0900

    숫자 비교하기

---
+++ 코딩테스트/yeojin/숫자 비교하기/다른 사람의 풀이.js
@@ -0,0 +1,9 @@
+function solution(num1, num2) {
+    var answer = num1 === num2 ? 1 : -1;
+    return answer;
+}
+
+/*
+  === : 엄격 비교 연산자, 타입과 값 모두 같아야 함
+  `조건 ? 참일 때 실행 : 거짓일때 실행` : 삼항연산자, 조건의 결과에 따라 실행이 달라짐
+*/
