c6f912dfffea8ede4c5be97a607cf517b553542b
xy-jxn <s24016@gsm.hs.kr>
2024-09-08 13:35:51 +0900

    접두사인지 확인하기

---
+++ 코딩테스트/yeojin/접두사인지 확인하기/나의 풀이.js
@@ -0,0 +1,7 @@
+function solution(my_string, is_prefix) {
+  return my_string.startsWith(is_prefix) ? 1 : 0;
+}
+
+/*
+  startsWith() : 해당 문자열로 시작하는지 확인하여 true 또는 false를 반환한다.
+*/
---
+++ 코딩테스트/yeojin/접두사인지 확인하기/다른 사람의 풀이.js
@@ -0,0 +1,7 @@
+function solution(my_string, is_prefix) {
+  return +my_string.startsWith(is_prefix);
+}
+
+/*
+  단항 덧셈 연산자(+) : boolean 값에 붙이면 true는 1, false는 0을 반환한다.
+*/
