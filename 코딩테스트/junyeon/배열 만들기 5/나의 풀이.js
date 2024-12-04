3687303e26b7da10b216d4987a16464cb229be68
junjuny0227 <junjuny0227@gmail.com>
2024-12-04 14:59:15 +0900

    배열 만들기 5

---
+++ 코딩테스트/junyeon/배열 만들기 5/나의 풀이.js
@@ -0,0 +1,6 @@
+function solution(intStrs, k, s, l) {
+  let answer = [];
+  for (const intStr of intStrs)
+    if (+intStr.slice(s, s + l) > k) answer.push(+intStr.slice(s, s + l));
+  return answer;
+}
