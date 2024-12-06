ec1f85d6a23c61a75d5a0c1bbe09c4a56d403ed3
junjuny0227 <junjuny0227@gmail.com>
2024-12-06 09:22:01 +0900

    중복된 문자 제거

---
+++ 코딩테스트/junyeon/중복된 문자 제거/나의 풀이.js
@@ -0,0 +1,7 @@
+function solution(my_string) {
+  let answer = my_string.split("");
+  for (let i = 0; i < answer.length; i++)
+    for (let j = i + 1; j < answer.length; j++)
+      if (answer[j] === answer[i]) answer[j] = "";
+  return answer.join("");
+}
