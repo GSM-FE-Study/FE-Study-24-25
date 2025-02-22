6b2b65205a0ef2833539d9a98554d8bc83901ea2
bae080311 <s24024@gsm.hs.kr>
2025-02-22 19:01:44 +0900

    이상한 문자 만들기기

---
+++ 코딩테스트/gyeongjin/이상한 문자 만들기/나의 풀이.js
@@ -0,0 +1,11 @@
+function solution(s) {
+  return s
+    .split(" ")
+    .map((v) =>
+      v
+        .split("")
+        .map((val, i) => (i % 2 === 0 ? val.toUpperCase() : val.toLowerCase()))
+        .join("")
+    )
+    .join(" ");
+}
