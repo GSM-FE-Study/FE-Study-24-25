0c1589c6562789ddc2f6c3f5c84c697fb6dfac3d
Dino0204 <s24020@gsm.hs.kr>
2025-04-17 22:57:28 +0900

    피보나치 수

---
+++ 코딩테스트/joonhyeok/피보나치 수/다른 사람의 풀이.js
@@ -0,0 +1,9 @@
+function solution(n) {
+  var result = [0, 1];
+  while (result.length !== n + 1) {
+    var fibonacci =
+      (result[result.length - 2] + result[result.length - 1]) % 1234567;
+    result.push(fibonacci);
+  }
+  return result[n];
+}
